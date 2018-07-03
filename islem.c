838b617c0a773c6fe9dfb6ce389797d3cb9d5cb7
İbrahim Halil ÜNLÜK <ih.unluk@alparslan.edu.tr>
2018-07-03 08:52:26 +0300

    islem.c dosyasındaki değişikler kaydedildi...

--- islem.c
+++ islem.c
@@ -11,18 +11,30 @@ s=a-b;
 printf("\n\n a=%d ve b=%d çikarma sonucu = %d \n\n",a,b,s);
 }
 
+int carp(){
+s=a*b;
+printf("\n\n a=%d ve b=%d çarpma sonucu = %d \n\n",a,b,s);
+}
 
 int main(){
 	
-printf("\n\nLütfen Toplama=1 Çikarma=2 değerini giriniz : ");
+printf("\n\nLütfen Toplama=1 Çikarma=2 Çarpma=3 Çıkış=0 değerini giriniz : ");
 scanf("%d",&i);
 
 	if(i==1){
 		topla();
+		main();
 	}else if(i==2){
 		cikar();
+		main();
+	}else if(i==3){
+		carp();
+		main();
+	}else if(i==0){
+		printf("\n\nİyi Çalışmalar...\n\n");
 	}else{
 		printf("\n\nLütfen geçerli bir işlem seçiniz...\n\n");
+		main();
 	}
 
 }
